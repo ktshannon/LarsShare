76ef8f317f74a5f35755bb03de64e8518387ac68
Lars Kluge <l@larskluge.com>
2016-03-24 10:51:05 -0600

    Parameter for web ui, concurrency, and request limit & do not run successful entries again

--- actions/get_entries.rb
+++ actions/get_entries.rb
@@ -3,13 +3,14 @@ class GetEntries < Dynflow::Action
 
   def plan ids
     puts "GetEntries#plan"
-    limit_concurrency_level 5
+    limit_concurrency_level REQUEST_CONCURRENCY
     super(ids: ids)
   end
 
   def create_sub_plans
     puts "GetEntries#create_sub_plans"
-    input[:ids][0..10].map do |id|
+    captured = Dir['data/entries/*.json'].map { |f| $1.to_i if f =~ /(\d+)\.json$/ }
+    (input[:ids].reverse - captured)[0..REQUEST_LIMIT].map do |id|
       trigger(GetEntry, id)
     end
   end
--- app
+++ app
@@ -9,5 +9,15 @@ require 'csv'
 require_relative './world'
 Dir['actions/*.rb'].each { |f| require_relative f }
 
+WEB_UI = true
+REQUEST_CONCURRENCY = 5
+REQUEST_LIMIT = 10            # -1 for unlimited
+
 World.world.action_logger.level = Logger::INFO
-World.world.trigger(CreateFeed).finished.wait
+triggered = World.world.trigger(CreateFeed)
+
+if WEB_UI
+  World.run_web_console
+else
+  triggered.finished.wait
+end
